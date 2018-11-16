d354627148bc4fd267845d6aef0a9d28ecff029d
MrTrainCow <legoguy145@gmail.com>
2018-11-16 09:52:33 -0500

    added api_url to arguments

--- oauth2.py
+++ oauth2.py
@@ -1,13 +1,13 @@
 import json
 from time import time
 import requests
-APIurl = 'https://api.regattacentral.com'
 
 class OAuth2:
-    # The OAuth class handles OAuth2 Authentication
-    def __init__(self, client_id, client_secret, username, password):
-        # This defines local variables
+    # The OAuth2 class handles OAuth2 Authentication
+    def __init__(self, api_url, client_id, client_secret, username, password):
         # client credentials
+        # username and password should be user input
+        # client_id and client_secret should be hardcoded
         self.client_id = client_id
         self.client_secret = client_secret
         self.username = username
@@ -20,14 +20,14 @@ class OAuth2:
     def get_token(self):
         # This requests and receives a token from the server
         # returns access token
-        print('token requested')
-        # get a token
+
+        # requests a token
         try:
-            t = json.loads(requests.post(APIurl+'/oauth2/api/token', data = {'client_id':self.client_id,'client_secret':self.client_secret,'username':self.username,'password':self.password,'grant_type':'password'}).text)
-        # Catch the error
+            t = json.loads(requests.post(self.api_url+'/oauth2/api/token', data = {'client_id':self.client_id,'client_secret':self.client_secret,'username':self.username,'password':self.password,'grant_type':'password'}).text)
+        # If a token is not issued, catch the error
         except json.decoder.JSONDecodeError:
-            print(requests.post(APIurl+'/oauth2/api/token', data = {'client_id':self.client_id,'client_secret':self.client_secret,'username':self.username,'password':self.password,'grant_type':'password'}).text)
-        # write token to variables
+            print(requests.post(self.api_url+'/oauth2/api/token', data = {'client_id':self.client_id,'client_secret':self.client_secret,'username':self.username,'password':self.password,'grant_type':'password'}).text)
+        # write token to memory
         print('token received')
         self.access_token = t['access_token']
         self.expires_at = t['expires_in'] + time()
@@ -37,13 +37,13 @@ class OAuth2:
 
     def refresh_token(self):
         # This will refresh the token if the token has expired
-        # returns nothing, write the token to variables
+        # returns nothing, write the token to memory
         print('refresh token')
         # refresh token after an hour
         try:
-            t = json.loads(requests.post(APIurl+'/oauth2/api/token', data = {'client_id':self.client_id,'client_secret':self.client_secret, 'refresh_token':self.refresh_token, 'grant_type':'refresh_token'}).text)
+            t = json.loads(requests.post(self.api_url+'/oauth2/api/token', data = {'client_id':self.client_id,'client_secret':self.client_secret, 'refresh_token':self.refresh_token, 'grant_type':'refresh_token'}).text)
         except json.decoder.JSONDecodeError:
-            print(requests.post(APIurl+'/oauth2/api/token', data = {'client_id':self.client_id,'client_secret':self.client_secret,'refresh_token':self.refresh_token,'grant_type':'refresh_token'}).text)
+            print(requests.post(self.api_url+'/oauth2/api/token', data = {'client_id':self.client_id,'client_secret':self.client_secret,'refresh_token':self.refresh_token,'grant_type':'refresh_token'}).text)
         else:
             self.expires_at = t['expires_in'] + time()
             self.access_token = t['access_token']
@@ -51,8 +51,9 @@ class OAuth2:
     def validate_token(self):
         # This will check if the token is valid or not
         # returns a boolean based on the validity of the token
-        t = json.loads(requests.post(APIurl+'/oauth2/api/validate', data = {'token':self.access_token}).text)
+        t = json.loads(requests.post(self.api_url+'/oauth2/api/validate', data = {'token':self.access_token}).text)
         if 'error' in t:
           return False
         else:
           return True
+
